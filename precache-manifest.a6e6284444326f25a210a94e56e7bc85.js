6afb63f3307e36aafbccac76cc8a5a9acb9d18f7
modakshantanu <modakshantanu@gmail.com>
2019-07-10 15:55:26 +0800

    Updates

---
+++ precache-manifest.a6e6284444326f25a210a94e56e7bc85.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90bb71a178ecbe7c1613bdfbcf1b7a9f",
+    "url": "/asteroids/index.html"
+  },
+  {
+    "revision": "418be1f8af24487b1bc3",
+    "url": "/asteroids/static/css/main.3ce96bb7.chunk.css"
+  },
+  {
+    "revision": "ecdf7d98951bebef99dd",
+    "url": "/asteroids/static/js/2.9aef3cd5.chunk.js"
+  },
+  {
+    "revision": "418be1f8af24487b1bc3",
+    "url": "/asteroids/static/js/main.d40d4a07.chunk.js"
+  },
+  {
+    "revision": "047f25bcae058dbce6db",
+    "url": "/asteroids/static/js/runtime~main.f2effb4d.js"
+  }
+]);
